a58e2b9381aa9442c044948e251693d0c23bcab8
lampert309 <lampert309@gmail.com>
2019-02-08 10:46:25 -0800

    Fininshed the display for frmEvent

--- Greed/frmEvent.Designer.cs
+++ Greed/frmEvent.Designer.cs
@@ -23,12 +23,252 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblMoney = new System.Windows.Forms.Label();
+            this.tbxChar1Health = new System.Windows.Forms.TextBox();
+            this.tbxChar2Health = new System.Windows.Forms.TextBox();
+            this.lblChar3Health = new System.Windows.Forms.TextBox();
+            this.lblChar4Health = new System.Windows.Forms.TextBox();
+            this.lblPartyName = new System.Windows.Forms.Label();
+            this.lblChar1Name = new System.Windows.Forms.Label();
+            this.lblChar2Name = new System.Windows.Forms.Label();
+            this.lblChar3Name = new System.Windows.Forms.Label();
+            this.lblChar4Name = new System.Windows.Forms.Label();
+            this.btnBagOLoot = new System.Windows.Forms.Button();
+            this.lblSkeleton = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 40);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 271);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lblMoney
+            // 
+            this.lblMoney.AutoSize = true;
+            this.lblMoney.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMoney.Location = new System.Drawing.Point(371, 236);
+            this.lblMoney.Name = "lblMoney";
+            this.lblMoney.Size = new System.Drawing.Size(69, 20);
+            this.lblMoney.TabIndex = 1;
+            this.lblMoney.Text = "Gold: 0g";
+            // 
+            // tbxChar1Health
+            // 
+            this.tbxChar1Health.Enabled = false;
+            this.tbxChar1Health.Location = new System.Drawing.Point(12, 68);
+            this.tbxChar1Health.Name = "tbxChar1Health";
+            this.tbxChar1Health.Size = new System.Drawing.Size(80, 20);
+            this.tbxChar1Health.TabIndex = 2;
+            this.tbxChar1Health.Text = "|||||||||||||||||||||||||||||||||||";
+            // 
+            // tbxChar2Health
+            // 
+            this.tbxChar2Health.Location = new System.Drawing.Point(12, 118);
+            this.tbxChar2Health.Name = "tbxChar2Health";
+            this.tbxChar2Health.Size = new System.Drawing.Size(80, 20);
+            this.tbxChar2Health.TabIndex = 3;
+            this.tbxChar2Health.Text = "|||||||||||||||||||||||||||||||||||";
+            // 
+            // lblChar3Health
+            // 
+            this.lblChar3Health.Location = new System.Drawing.Point(12, 168);
+            this.lblChar3Health.Name = "lblChar3Health";
+            this.lblChar3Health.Size = new System.Drawing.Size(80, 20);
+            this.lblChar3Health.TabIndex = 4;
+            this.lblChar3Health.Text = "|||||||||||||||||||||||||||||||||||";
+            // 
+            // lblChar4Health
+            // 
+            this.lblChar4Health.Location = new System.Drawing.Point(12, 218);
+            this.lblChar4Health.Name = "lblChar4Health";
+            this.lblChar4Health.Size = new System.Drawing.Size(80, 20);
+            this.lblChar4Health.TabIndex = 5;
+            this.lblChar4Health.Text = "|||||||||||||||||||||||||||||||||||";
+            // 
+            // lblPartyName
+            // 
+            this.lblPartyName.AutoSize = true;
+            this.lblPartyName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPartyName.Location = new System.Drawing.Point(12, 17);
+            this.lblPartyName.Name = "lblPartyName";
+            this.lblPartyName.Size = new System.Drawing.Size(91, 20);
+            this.lblPartyName.TabIndex = 6;
+            this.lblPartyName.Text = "Party Name";
+            // 
+            // lblChar1Name
+            // 
+            this.lblChar1Name.AutoSize = true;
+            this.lblChar1Name.Location = new System.Drawing.Point(13, 52);
+            this.lblChar1Name.Name = "lblChar1Name";
+            this.lblChar1Name.Size = new System.Drawing.Size(69, 13);
+            this.lblChar1Name.TabIndex = 7;
+            this.lblChar1Name.Text = "Char 1 Name";
+            // 
+            // lblChar2Name
+            // 
+            this.lblChar2Name.AutoSize = true;
+            this.lblChar2Name.Location = new System.Drawing.Point(9, 102);
+            this.lblChar2Name.Name = "lblChar2Name";
+            this.lblChar2Name.Size = new System.Drawing.Size(69, 13);
+            this.lblChar2Name.TabIndex = 8;
+            this.lblChar2Name.Text = "Char 2 Name";
+            // 
+            // lblChar3Name
+            // 
+            this.lblChar3Name.AutoSize = true;
+            this.lblChar3Name.Location = new System.Drawing.Point(9, 152);
+            this.lblChar3Name.Name = "lblChar3Name";
+            this.lblChar3Name.Size = new System.Drawing.Size(69, 13);
+            this.lblChar3Name.TabIndex = 9;
+            this.lblChar3Name.Text = "Char  Name3";
+            // 
+            // lblChar4Name
+            // 
+            this.lblChar4Name.AutoSize = true;
+            this.lblChar4Name.Location = new System.Drawing.Point(9, 202);
+            this.lblChar4Name.Name = "lblChar4Name";
+            this.lblChar4Name.Size = new System.Drawing.Size(69, 13);
+            this.lblChar4Name.TabIndex = 10;
+            this.lblChar4Name.Text = "Char 4 Name";
+            // 
+            // btnBagOLoot
+            // 
+            this.btnBagOLoot.Location = new System.Drawing.Point(656, 236);
+            this.btnBagOLoot.Name = "btnBagOLoot";
+            this.btnBagOLoot.Size = new System.Drawing.Size(75, 75);
+            this.btnBagOLoot.TabIndex = 11;
+            this.btnBagOLoot.Text = "Bag O\' Loot";
+            this.btnBagOLoot.UseVisualStyleBackColor = true;
+            // 
+            // lblSkeleton
+            // 
+            this.lblSkeleton.AutoSize = true;
+            this.lblSkeleton.Location = new System.Drawing.Point(12, 252);
+            this.lblSkeleton.Name = "lblSkeleton";
+            this.lblSkeleton.Size = new System.Drawing.Size(49, 13);
+            this.lblSkeleton.TabIndex = 12;
+            this.lblSkeleton.Text = "Skeletan";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 268);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(80, 20);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "|||||||||||||||||||||||||||||||||||";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(375, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 75);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(556, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 75);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(375, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 75);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(556, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(175, 75);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDay.Location = new System.Drawing.Point(142, 17);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(126, 20);
+            this.lblDay.TabIndex = 18;
+            this.lblDay.Text = "Morning of day 1";
+            // 
+            // frmEvent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(759, 336);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblSkeleton);
+            this.Controls.Add(this.btnBagOLoot);
+            this.Controls.Add(this.lblChar4Name);
+            this.Controls.Add(this.lblChar3Name);
+            this.Controls.Add(this.lblChar2Name);
+            this.Controls.Add(this.lblChar1Name);
+            this.Controls.Add(this.lblPartyName);
+            this.Controls.Add(this.lblChar4Health);
+            this.Controls.Add(this.lblChar3Health);
+            this.Controls.Add(this.tbxChar2Health);
+            this.Controls.Add(this.tbxChar1Health);
+            this.Controls.Add(this.lblMoney);
+            this.Controls.Add(this.textBox1);
+            this.Name = "frmEvent";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmEvent";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblMoney;
+        private System.Windows.Forms.TextBox tbxChar1Health;
+        private System.Windows.Forms.TextBox tbxChar2Health;
+        private System.Windows.Forms.TextBox lblChar3Health;
+        private System.Windows.Forms.TextBox lblChar4Health;
+        private System.Windows.Forms.Label lblPartyName;
+        private System.Windows.Forms.Label lblChar1Name;
+        private System.Windows.Forms.Label lblChar2Name;
+        private System.Windows.Forms.Label lblChar3Name;
+        private System.Windows.Forms.Label lblChar4Name;
+        private System.Windows.Forms.Button btnBagOLoot;
+        private System.Windows.Forms.Label lblSkeleton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lblDay;
     }
 }
