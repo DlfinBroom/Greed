afeb8dfe04838f4b80cd215dd210c5ec6583addc
NessRK13 <ness6514@student.cptc.edu>
2019-02-22 10:57:58 -0800

    Created the Loot Class, the IsArmor property might be subject to change.

--- Greed/Loot.cs
+++ Greed/Loot.cs
@@ -5,6 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Greed {
-    class Loot {
+    public class Loot {
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Determines if the loot is a weapon or armor
+        /// </summary>
+        public bool IsArmor { get; set; }
+
+        /// <summary>
+        /// Ex. Axe, Bow, Light armor, etc.
+        /// </summary>
+        public string LootType { get; set; }
+
+        public byte Level { get; set; }
+
+        public string MagicStat { get; set; }
+
+        public byte MagicStatIncr { get; set; }
+
+        /// <summary>
+        /// How good of quality the weapons and armor are
+        /// Ex. 5 levels, 0 - 4
+        /// </summary>
+        public byte Quality { get; set; }
     }
 }
