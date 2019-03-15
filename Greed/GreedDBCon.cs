d8e7753b47fde10bbfde21ca70f4c6b4859043b6
lampert309 <lampert309@gmail.com>
2019-03-15 10:09:07 -0700

    Made a GreedDbCon class and got lootDB workin i think

---
+++ Greed/GreedDBCon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greed {
+    static class GreedDBCon {
+        public static SqlConnection GetConnection() {
+            string connectionString = 
+                "(localdb)\\ProjectsV13; Initial Catalog = GreedDB;" +
+                " Integrated Security = True; Connect Timeout = 30;" +
+                " Encrypt = False; TrustServerCertificate = False;" +
+                " ApplicationIntent = ReadWrite;" +
+                " MultiSubnetFailover = False";
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = connectionString;
+            return con;
+        }
+    }
+}
